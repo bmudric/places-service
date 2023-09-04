84cdfa638a56d4da05d731c5e22a427dd1b19b96
Bojan Mudric <14099136+bmudric@users.noreply.github.com>
2023-09-04 17:33:11 +0200

    Initial commit
    Project generated using spring initializr start.spring.io

---
+++ README.md
@@ -0,0 +1,1 @@
+# Assignment Places Service
---
+++ src/test/java/com/example/assignment/placesservice/PlacesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment.placesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
